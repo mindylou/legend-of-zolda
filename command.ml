a8ae68196cee38ba5ef79e17282e40b3eacec170
Alexander Sabia <as2633@cornell.edu>
2017-11-13 20:06:55 -0500

    get_key_state comment

--- command.ml
+++ command.ml
@@ -5,3 +5,8 @@ type command = {w: bool;
                 j: bool;
                 k: bool;
                 l: bool}
+
+(* let keystates = get_key_state () in
+   if keystates.{ int_of_key KEY_UP } <> 0 then *)
+               
+(* Up key was pressed *) 
