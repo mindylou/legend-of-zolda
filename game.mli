9cb9d31737f4cedfa855b7decb08d67625498c72
Mindy Lou <mml234@cornell.edu>
2017-12-06 20:12:08 -0500

    clean up

--- game.mli
+++ game.mli
@@ -1,13 +1,11 @@
 open Types
 
-(* [json_to_init_state j] reads in the json from the given file name and
-   initializes the state. *)
-(* val json_to_init_state: string -> state *)
-
 (* [game_loop context st has_won] controls the game loop and draws the updates
    to the canvas *)
 val game_loop: Dom_html.canvasRenderingContext2D Js.t -> bool -> unit
 
+(* [keydown event] registers when a key has been pressed. *)
 val keydown : Dom_html.keyboardEvent Js.t -> bool Js.t
 
+(* [keyup event] registers when a key has been lifted. *)
 val keyup : Dom_html.keyboardEvent Js.t -> bool Js.t
--- gui.mli
+++ gui.mli
@@ -6,20 +6,5 @@ val canvas_width: float
 (* drawing canvas height *)
 val canvas_height: float
 
-val win_screen : Dom_html.canvasRenderingContext2D Js.t -> unit
-
-(* [draw_image_on_context context img_src x y] draws the given [img_src]
-   string at the x,y [coord] on the canvas' [context]. *)
-val draw_image_on_context: Dom_html.canvasRenderingContext2D Js.t ->
-  Js.js_string Js.t -> (float * float) -> unit
-
-val draw_objects: Dom_html.canvasRenderingContext2D Js.t -> obj list -> unit
-
-val clear: Dom_html.canvasRenderingContext2D Js.t -> unit
-
-val update_animations: sprite -> unit
-
-val update_all_animations: sprite list -> unit
-
 (* [draw_state canvas state] draws the current state onto the [canvas]. *)
 val draw_state: Dom_html.canvasRenderingContext2D Js.t -> state -> unit
